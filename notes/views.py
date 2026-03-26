10cfa4ea1ca8d91f1de91b7d7fcd2624f53678e6
Anna <anet.kravchenko@gmail.com>
2026-03-26 18:01:03 +0200

    Add ORM with test data

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Note
 
 def hello(request):
-    return render(request, 'index.html')
+    notes = Note.objects.all()
+    return render(request, 'index.html', {'notes': notes})
